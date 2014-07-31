85493aa661af12163e226dddc4d258c4b3e7ed22
holyshared <holy.shared.design@gmail.com>
2014-07-31 09:29:00 +0900

    add report factory

---
+++ spec/report/LcovReportFactorySpec.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of cloak.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+use cloak\report\LcovReportFactory;
+use cloak\Result;
+
+describe('LcovReportFactory', function() {
+    describe('#createFromResult', function() {
+        before(function() {
+            $this->result = Result::from([]);
+            $this->reportFactory = new LcovReportFactory();
+            $this->report = $this->reportFactory->createFromResult($this->result);
+        });
+    });
+});
